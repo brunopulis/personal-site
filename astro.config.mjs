f697bbf1fb9d4b14db2f10b8b6cd9258b4fa0c61
Bruno Pulis <brunopulis@protonmail.com>
2025-05-18 16:20:54 -0300

    fix guestbook

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import db from '@astrojs/db';
+import db from "@astrojs/db";
 import pagefind from "astro-pagefind";
 import tailwindcss from "@tailwindcss/vite";
 import alpinejs from '@astrojs/alpinejs';
