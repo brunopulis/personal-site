176e958b5e711a2c23654373d7aba132b6772d70
Bruno Pulis <brunopulis@protonmail.com>
2025-05-18 13:59:41 -0300

    chore: add astro db dependencies

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,1 +1,10 @@
-import { defineDb, defineTable, column } from 'astro:db'
+import { defineDb, defineTable, column, NOW } from 'astro:db';
+
+const Guestbook = defineTable({
+  name: 'guestbook',
+  columns: {
+    id: column.integer().primaryKey().autoIncrement(),
+    author: column.text(),
+    timestamp: column.date({ default: NOW }),
+  }
+});
