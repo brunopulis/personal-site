4a907f733a29d9f77f1b7e4a759132ec0993f755
Bruno Pulis <brunopulis@protonmail.com>
2025-10-26 17:39:18 -0300

    fix bad format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,75 +1,75 @@
 // @ts-check
-import { defineConfig } from 'astro/config'
+
+import alpinejs from "@astrojs/alpinejs";
 
 import db from "@astrojs/db";
-import netlify from '@astrojs/netlify'
-import partytown from '@astrojs/partytown'
-import sitemap from '@astrojs/sitemap'
-import compress from 'astro-compress'
-import tailwindcss from '@tailwindcss/vite'
-import alpinejs from '@astrojs/alpinejs'
-import icon from 'astro-icon'
-import react from '@astrojs/react'
-import mdx from '@astrojs/mdx'
-import umami from '@yeskunall/astro-umami'
-import robotsTxt from 'astro-robots-txt'
+import mdx from "@astrojs/mdx";
+import netlify from "@astrojs/netlify";
+import partytown from "@astrojs/partytown";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import umami from "@yeskunall/astro-umami";
+import { defineConfig } from "astro/config";
+import compress from "astro-compress";
+import icon from "astro-icon";
+import robotsTxt from "astro-robots-txt";
 
 export default defineConfig({
-  integrations: [
-    react(),
-    db(),
-    icon(),
-    alpinejs(),
-    sitemap(),
-    mdx(),
-    compress(),
-    partytown({
-      config: {
-        forward: ['dataLayer.push'],
-      },
-    }),
-    umami({ id: 'f1cb7c07-2efc-4997-9dd3-d3198f0faa0c' }),
-    robotsTxt({
-      policy: [
-        {
-          userAgent: 'CCBot',
-          disallow: '/',
-        },
-        {
-          userAgent: 'ChatGPT-User',
-          disallow: '/',
-        },
-        {
-          userAgent: 'GPTBot',
-          disallow: '/',
-        },
-        {
-          userAgent: 'Google-Extended',
-          disallow: '/',
-        },
-        {
-          userAgent: 'Omgilibot',
-          disallow: '/',
-        },
-        {
-          userAgent: 'FacebookBot',
-          disallow: '/',
-        },
-      ],
-    }),
-
-  ],
-  adapter: netlify(),
-  output: 'server',
-  site: 'https://brunopulis.com',
-  build: {
-    assets: 'assets',
-  },
-  compressHTML: true,
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  image: {
-    responsiveStyles: true,
-  },
-})
+	integrations: [
+		react(),
+		db(),
+		icon(),
+		alpinejs(),
+		sitemap(),
+		mdx(),
+		compress(),
+		partytown({
+			config: {
+				forward: ["dataLayer.push"],
+			},
+		}),
+		umami({ id: "f1cb7c07-2efc-4997-9dd3-d3198f0faa0c" }),
+		robotsTxt({
+			policy: [
+				{
+					userAgent: "CCBot",
+					disallow: "/",
+				},
+				{
+					userAgent: "ChatGPT-User",
+					disallow: "/",
+				},
+				{
+					userAgent: "GPTBot",
+					disallow: "/",
+				},
+				{
+					userAgent: "Google-Extended",
+					disallow: "/",
+				},
+				{
+					userAgent: "Omgilibot",
+					disallow: "/",
+				},
+				{
+					userAgent: "FacebookBot",
+					disallow: "/",
+				},
+			],
+		}),
+	],
+	adapter: netlify(),
+	output: "server",
+	site: "https://brunopulis.com",
+	build: {
+		assets: "assets",
+	},
+	compressHTML: true,
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	image: {
+		responsiveStyles: true,
+	},
+});
