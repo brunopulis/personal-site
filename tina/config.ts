571f3eb2d83d9f64a5aded95c6024c708b9e2677
Bruno Pulis <brunopulis@protonmail.com>
2025-11-01 22:36:46 -0300

    chore: tina

--- tina/config.ts
+++ tina/config.ts
@@ -1,4 +1,11 @@
 import { defineConfig } from "tinacms";
+import { BlogCollection } from "./collections/blog";
+import { BlogrollCollection } from "./collections/blogroll";
+import { NotasCollection } from "./collections/notas";
+import { BibliotecaCollection } from "./collections/books";
+import { MoviesCollection } from "./collections/movies";
+import { NewsletterCollection } from "./collections/newsletter";
+import { SpeakingCollection } from "./collections/speaking";
 
 const branch =
   process.env.GITHUB_BRANCH ||
@@ -23,179 +30,13 @@ export default defineConfig({
   },
   schema: {
     collections: [
-      {
-        name: "bookmark",
-        label: "Bookmark",
-        path: "src/content/bookmarks",
-        fields: [
-          {
-            type: "string",
-            name: "title",
-            label: "Title",
-            isTitle: true,
-            required: true,
-          },
-          {
-            type: 'datetime',
-            name: 'date',
-            label: 'Date'
-          },
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body",
-            isBody: true,
-          },
-          {
-            type: "string",
-            name: "url",
-            label: "URL",
-            required: true,
-          },
-        ],
-      },
-      {
-        name: "post",
-        label: "Posts",
-        path: "src/content/blog",
-        fields: [
-          {
-            type: "string",
-            name: "title",
-            label: "Title",
-            isTitle: true,
-            required: true,
-          },
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body",
-            isBody: true,
-          },
-        ],
-      },
-      {
-        name: "book",
-        label: "Books",
-        path: "src/content/books",
-        fields: [
-          {
-            type: "string",
-            name: "title",
-            label: "Title",
-            isTitle: true,
-            required: true,
-          },
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body",
-            isBody: true,
-          },
-        ],
-      },
-      {
-        name: "note",
-        label: "Notas",
-        path: "src/content/notes",
-        fields: [
-          {
-            type: "string",
-            name: "title",
-            label: "Title",
-            isTitle: true,
-            required: true,
-          },
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body",
-            isBody: true,
-          },
-        ],
-      },
-      {
-        name: "movie",
-        label: "Meus filmes",
-        path: "src/content/movies",
-        fields: [
-          {
-            type: "string",
-            name: "title",
-            label: "Title",
-            isTitle: true,
-            required: true,
-          },
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body",
-            isBody: true,
-          },
-        ],
-      },
-      {
-        name: "newsletter",
-        label: "Newsletter",
-        path: "src/content/newsletter",
-        fields: [
-          {
-            type: "string",
-            name: "title",
-            label: "Title",
-            isTitle: true,
-            required: true,
-          },
-          {
-            type: "string",
-            name: "issue",
-            label: "Edição",
-            required: true
-          },
-          {
-            type: "image",
-            name: "coverImage",
-            label: "Hero"
-          },
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body",
-            isBody: true,
-          },
-        ],
-      },
-      {
-        name: "speaking",
-        label: "Palestras",
-        path: "src/content/speaking",
-        fields: [
-          {
-            type: "string",
-            name: "title",
-            label: "Title",
-            isTitle: true,
-            required: true,
-          },
-          {
-            type: "string",
-            name: "issue",
-            label: "Edição",
-            required: true
-          },
-          {
-            type: "image",
-            name: "coverImage",
-            label: "Hero"
-          },
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body",
-            isBody: true,
-          },
-        ],
-      },
+      BlogCollection,
+      NotasCollection,
+      BlogrollCollection,
+      BibliotecaCollection,
+      MoviesCollection,
+      NewsletterCollection,
+      SpeakingCollection
     ],
   },
   search: {
