ffbe92008eb656b3c442d8c159cf4a733c8f5bbf
Bruno Pulis <brunopulis@protonmail.com>
2025-11-09 16:27:30 -0300

    fix: urls pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,11 @@ import compress from "astro-compress";
 import icon from "astro-icon";
 import robotsTxt from "astro-robots-txt";
 
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   adapter: vercel({ static: true }),
   output: "static",
@@ -63,6 +68,16 @@ export default defineConfig({
   trailingSlash: 'ignore',
   compressHTML: true,
   vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@components': path.resolve(__dirname, './src/components'),
+        '@layouts': path.resolve(__dirname, './src/layouts'),
+        '@assets': path.resolve(__dirname, './src/assets'),
+        '@scripts': path.resolve(__dirname, './src/scripts'),
+        '@styles': path.resolve(__dirname, './src/styles')
+      }
+    },
     plugins: [tailwindcss()],
     server: {
       fs: {
