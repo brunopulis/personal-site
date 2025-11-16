261f98cca77bc2684c39a0c525b4a85e1429e1ba
Bruno Pulis <brunopulis@protonmail.com>
2025-11-16 01:01:28 -0300

    add feeds rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
@@ -6,36 +5,16 @@ export async function GET(context) {
   const blog = await getCollection('blog');
   
   return rss({
-    stylesheet: `/rss-styles.xsl`,
+    stylesheet: '/rss-styles.xsl',
     title: 'Meu Blog',
     description: 'Escrevo sobre acessibilidade, tecnologia, teologia, produtividade. Nem tudo é técnico, às vezes é só sobre a vida.',
     site: context.site,
     items: blog.map((post) => ({
       title: post.data.title,
       pubDate: post.data.publishDate,
-=======
-import rss from "@astrojs/rss";
-import { getCollection } from "astro:content";
-
-export async function GET(context) {
-  const posts = await getCollection("blog");
-
-  return rss({
-    stylesheet: `/rss-styles.xsl`,
-    title: "Meu blog",
-    description: "Escrevo sobre acessibilidade, tecnologia, teologia, produtividade. Nem tudo é técnico, às vezes é só sobre a vida.",
-    site: context.site,
-    items: posts.map(post => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
->>>>>>> main
       description: post.data.description,
       link: `/blog/${post.slug}/`,
     })),
     customData: `<language>pt-br</language>`,
   });
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> main
