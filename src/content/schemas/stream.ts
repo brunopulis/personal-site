4494c04c079dc6ed3df72f0cafdc341e8f67eb94
Bruno Pulis <brunopulis@protonmail.com>
2025-11-20 17:48:29 -0300

    fix issues on stream

--- src/content/schemas/stream.ts
+++ src/content/schemas/stream.ts
@@ -1,11 +1,13 @@
 import { defineCollection, z } from "astro:content";
+import { seoSchema } from "./seo";
 
 export const stream = defineCollection({
   schema: z.object({
     title: z.string(),
     type: z.enum(['book', 'music', 'film', 'podcast', 'concert', 'link', 'note']),
     detail: z.string().optional(),
-    pubDate: z.date(),
+    pubDate: z.coerce.date(),
     tags: z.array(z.string()).optional(),
+    seo: seoSchema.optional(),
   }),
 });
