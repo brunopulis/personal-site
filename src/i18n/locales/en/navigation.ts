599aff6bf4eded8776d8fb7694002990cc249ef9
Bruno Pulis <brunopulis@protonmail.com>
2025-11-20 16:51:33 -0300

    add i18n support

---
+++ src/i18n/locales/en/navigation.ts
@@ -0,0 +1,8 @@
+export const enNavigation = {
+  "nav.about": "about",
+  "nav.now": "now",
+  "nav.blog": "writing",
+  "nav.stream": "stream",
+  "nav.work": "work",
+  "nav.contact": "contact",
+};
---
+++ src/i18n/locales/pt-br/navigation.ts
@@ -0,0 +1,8 @@
+export const ptBrNavigation = {
+  "nav.about": "sobre",
+  "nav.now": "agora",
+  "nav.blog": "escrevendo",
+  "nav.stream": "vida",
+  "nav.work": "trabalho",
+  "nav.contact": "contato",
+};
