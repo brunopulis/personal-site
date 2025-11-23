f15cba8bd1aac806500b5f1a28cde3b2ca0ba7b9
Bruno Pulis <brunopulis@protonmail.com>
2025-11-23 12:04:35 -0300

    update: schema for notes

--- src/content/schemas/notes.ts
+++ src/content/schemas/notes.ts
@@ -2,7 +2,18 @@ import { defineCollection, z } from 'astro:content';
 
 export const notes = defineCollection({
   schema: z.object({
+    title: z.string().optional(),
     pubDate: z.coerce.date(),
-    tags: z.array(z.string()).default([]),
+    published: z.boolean().default(true),
+    type: z.enum(['note', 'reply', 'article']).default('note'),
+    syndication: z.array(z.string().url()).optional(),
+    tags: z.array(z.string()).optional(),
+    in_reply_to: z.string().url().optional(),
+    metrics: z
+      .object({
+        retweets: z.number(),
+        favorites: z.number(),
+      })
+      .optional(),
   }),
 });
