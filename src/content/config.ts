47496f120f61790090054d8b6892ecf626513784
Bruno Pulis <brunopulis@protonmail.com>
2025-11-23 12:04:56 -0300

    import tweets

---
+++ src/content/config.ts
@@ -0,0 +1,29 @@
+import {
+  biblioteca,
+  blog,
+  blogrolls,
+  bookmarks,
+  likes,
+  movies,
+  musics,
+  newsletter,
+  notes,
+  pages,
+  speaking,
+  stream,
+} from './schemas';
+
+export const collections = {
+  biblioteca,
+  blog,
+  blogrolls,
+  bookmarks,
+  likes,
+  movies,
+  musics,
+  newsletter,
+  notes,
+  pages,
+  speaking,
+  stream,
+};
