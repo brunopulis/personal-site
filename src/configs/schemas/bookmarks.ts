f775fa93e3b29ef1daa6d3f9f41e0cf686e8fcac
Bruno Pulis <brunopulis@protonmail.com>
2025-12-02 17:21:48 -0300

    remove configs

--- src/configs/schemas/bookmarks.ts
+++
@@ -1,8 +0,0 @@
-import { defineCollection, z } from 'astro:content';
-
-export const bookmarks = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    date: z.coerce.date(),
-  }),
-});
--- src/configs/schemas/speaking.ts
+++
@@ -1,8 +0,0 @@
-import { defineCollection, z } from 'astro:content';
-
-export const speaking = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    date: z.coerce.date(),
-  }),
-});
