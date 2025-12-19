a24e817d71c353c5032e910c435f553c66dc21a4
Bruno Pulis <brunopulis@protonmail.com>
2025-12-19 10:57:05 -0300

    add static media

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/home/pulis/Projetos/javascript/brunopulis/tina/__generated__/.cache/1766109552100', url: 'https://content.tinajs.io/2.0/content/6517e460-fcd1-43ef-99a2-7920ce8f947f/github/main', token: 'f3a1b2b67ef56872969cc0a18fc9f76d8d2373a7', queries,  });
+export const client = createClient({ cacheDir: '/home/pulis/Projetos/javascript/brunopulis/tina/__generated__/.cache/1766152362909', url: 'https://content.tinajs.io/2.0/content/33e6ec2a-b123-4160-9d67-f6a161d8814c/github/main', token: '17715609801b1d380d259cb38d3ba8a91a359d24', queries,  });
 export default client;
   
