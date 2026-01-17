3f447112d70f33318d3b68d61d501f6a5be7305b
Bruno Pulis <brunopulis@protonmail.com>
2026-01-17 17:08:37 -0300

    feat(rss): add support to RSS, Atom and JSON

---
+++ src/_data/metadata.js
@@ -0,0 +1,10 @@
+export default {
+  title: "Seu Site",
+  url: "https://www.brunopulis.com",
+  language: "pt-BR",
+  description: "Desenvolvedor e engenheiro de acessibilidade",
+  author: {
+    name: "Bruno Pulis",
+    email: "brunopulis@protonmail.com"
+  }
+};
