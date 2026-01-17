67c5498cdb95f0fa90a55d971aa4ee85dedd7a57
Bruno Pulis <brunopulis@protonmail.com>
2026-01-17 20:20:50 -0300

    refactor(gallery): add polaroid effect

---
+++ src/_data/gallery.js
@@ -0,0 +1,25 @@
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default function() {
+  const dir = path.join(__dirname, "../../assets/images/gallery/");
+
+
+  if (!fs.existsSync(dir)) {
+    console.warn(`Diretório não encontrado: ${dir}`);
+    return [];
+  }
+
+  const images = fs.readdirSync(dir).filter(file => {
+    return [".jpg", ".jpeg", ".png", ".webp"].includes(path.extname(file).toLowerCase());
+  });
+
+  return images.map(image => ({
+    src: `/assets/images/gallery/${image}`,
+    alt: ''
+  }));
+};
