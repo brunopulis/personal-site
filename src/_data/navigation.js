8e1e04192bd9b30c65dbaa2b1e50bde4e51e4c34
Bruno Pulis <brunopulis@protonmail.com>
2026-01-29 20:51:17 -0300

    clean data

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -24,49 +24,5 @@ export default {
 			text: 'Contato',
 			url: '/contato/'
 		}
-	],
-	secondary: [
-		{
-			text: 'Disclaimer',
-			url: '/disclaimer/'
-		},
-		{
-			text: 'Licença de uso',
-			url: '/licenca/'
-		},
-		{
-			text: 'Política de privacidade',
-			url: '/privacidade/'
-		},
-		{
-			text: 'Declaração de acessibilidade',
-			url: '/acessibilidade/'
-		}
-	],
-	bottom: [
-		{
-			text: 'Blogroll',
-			url: '/blogroll/'
-		},
-		{
-			text: 'Colophon',
-			url: '/colophon/'
-		},
-    {
-			text: 'O que uso',
-			url: '/uses/'
-		},
-    {
-			text: 'Meu uso de IA',
-			url: '/ia/'
-		},
-		{
-			text: 'Links favoritos',
-			url: '/bookmarks/'
-		},
-		{
-			text: 'Biblioteca',
-			url: '/livros/'
-		}
 	]
 };
