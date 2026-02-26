2bd2bea4dc3348f786f8f7a538eedbbf936f68ef
Bruno Pulis <brunopulis@protonmail.com>
2026-02-26 18:56:48 -0300

    refactor: manifest and sw

--- src/sw.js
+++ src/sw.js
@@ -5,31 +5,41 @@ const OFFLINE_URLS = [
   '/assets/css/index.css',
   '/assets/js/main.js',
   '/assets/favicon-32x32.png',
-  '/assets/apple-touch-icon.png'
+  '/assets/apple-touch-icon.png',
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(OFFLINE_URLS)).then(() => self.skipWaiting())
+    caches
+      .open(CACHE_NAME)
+      .then(cache => cache.addAll(OFFLINE_URLS))
+      .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.map((k) => (k === CACHE_NAME ? null : caches.delete(k))))).then(() => self.clients.claim())
+    caches
+      .keys()
+      .then(keys => Promise.all(keys.map(k => (k === CACHE_NAME ? null : caches.delete(k)))))
+      .then(() => self.clients.claim())
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   const { request } = event;
   if (request.method !== 'GET') return;
   event.respondWith(
-    caches.match(request).then((cached) =>
-      cached || fetch(request).then((resp) => {
-        const respClone = resp.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(request, respClone));
-        return resp;
-      }).catch(() => caches.match('/'))
+    caches.match(request).then(
+      cached =>
+        cached ||
+        fetch(request)
+          .then(resp => {
+            const respClone = resp.clone();
+            caches.open(CACHE_NAME).then(cache => cache.put(request, respClone));
+            return resp;
+          })
+          .catch(() => caches.match('/'))
     )
   );
 });
