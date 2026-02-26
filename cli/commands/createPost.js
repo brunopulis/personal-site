08171743867010609f2a966506139257e5bf00f4
Bruno Pulis <brunopulis@protonmail.com>
2026-02-26 19:01:07 -0300

    refactor codebase

--- cli/commands/createPost.js
+++ cli/commands/createPost.js
@@ -1,78 +1,78 @@
-import { input } from "@inquirer/prompts";
-import checkbox from "@inquirer/checkbox";
-import fs from 'fs';
-import path from 'path';
+import fs from 'node:fs';
+import checkbox from '@inquirer/checkbox';
+import { input } from '@inquirer/prompts';
 
-export default async(__siteroot) => {
-   const title = await input({ message: 'Post title' })
-    const slug = await input({ message: 'Post slug', default: utils.slugify(title) })
+export default async __siteroot => {
+  const title = await input({ message: 'Post title' });
+  const slug = await input({ message: 'Post slug', default: utils.slugify(title) });
 
-    const slugDate = new Date().toISOString().split('T')[0]
-    const year = new Date().getFullYear()
-    const postDate = new Date().toISOString()
+  const slugDate = new Date().toISOString().split('T')[0];
+  const year = new Date().getFullYear();
+  const postDate = new Date().toISOString();
 
-    const tags = await checkbox({
-        message: 'Tags',
-        choices: [
-            { name: 'ActivityPub', value: 'ActivityPub' },
-            { name: 'AdventOfCode', value: 'AdventOfCode' },
-            { name: 'AI', value: 'AI' },
-            { name: 'Alfred', value: 'Alfred' },
-            { name: 'Appearance', value: 'Appearance' },
-            { name: 'Apps', value: 'Apps' },
-            { name: 'BabyKnight', value: 'BabyKnight' },
-            { name: 'Development', value: 'Development' },
-            { name: 'DIY', value: 'DIY' },
-            { name: 'Eleventy', value: 'Eleventy' },
-            { name: 'EchoFeed', value: 'EchoFeed' },
-            { name: 'Food', value: 'Food' },
-            { name: 'Games', value: 'Games' },
-            { name: 'Hackathon', value: 'Hackathon' },
-            { name: 'Homescreen', value: 'Homescreen' },
-            { name: 'Lego', value: 'Lego' },
-            { name: 'Letters', value: 'Letters' },
-            { name: 'LinkDump', value: 'LinkDump' },
-            { name: 'MacOS', value: 'MacOS' },
-            { name: 'Mastodon', value: 'Mastodon' },
-            { name: 'Movies', value: 'Movies' },
-            { name: 'Music', value: 'Music' },
-            { name: 'OpenWeb', value: 'OpenWeb' },
-            { name: 'Personal', value: 'Personal' },
-            { name: 'Podcasting', value: 'Podcasting' },
-            { name: 'Raycast', value: 'Raycast' },
-            { name: 'Recipes', value: 'Recipes' },
-            { name: 'Shortcuts', value: 'Shortcuts' },
-            { name: 'SocialMedia', value: 'SocialMedia' },
-            { name: 'StJude', value: 'StJude' },
-            { name: 'ThingsILike', value: 'ThingsILike' },
-            { name: 'TV', value: 'TV' },
-            { name: 'WeblogPoMo', value: 'WeblogPoMo' },
-            { name: 'YearInReview', value: 'YearInReview' },
-        ],
-    })
+  const tags = await checkbox({
+    message: 'Tags',
+    choices: [
+      { name: 'ActivityPub', value: 'ActivityPub' },
+      { name: 'AdventOfCode', value: 'AdventOfCode' },
+      { name: 'AI', value: 'AI' },
+      { name: 'Alfred', value: 'Alfred' },
+      { name: 'Appearance', value: 'Appearance' },
+      { name: 'Apps', value: 'Apps' },
+      { name: 'BabyKnight', value: 'BabyKnight' },
+      { name: 'Development', value: 'Development' },
+      { name: 'DIY', value: 'DIY' },
+      { name: 'Eleventy', value: 'Eleventy' },
+      { name: 'EchoFeed', value: 'EchoFeed' },
+      { name: 'Food', value: 'Food' },
+      { name: 'Games', value: 'Games' },
+      { name: 'Hackathon', value: 'Hackathon' },
+      { name: 'Homescreen', value: 'Homescreen' },
+      { name: 'Lego', value: 'Lego' },
+      { name: 'Letters', value: 'Letters' },
+      { name: 'LinkDump', value: 'LinkDump' },
+      { name: 'MacOS', value: 'MacOS' },
+      { name: 'Mastodon', value: 'Mastodon' },
+      { name: 'Movies', value: 'Movies' },
+      { name: 'Music', value: 'Music' },
+      { name: 'OpenWeb', value: 'OpenWeb' },
+      { name: 'Personal', value: 'Personal' },
+      { name: 'Podcasting', value: 'Podcasting' },
+      { name: 'Raycast', value: 'Raycast' },
+      { name: 'Recipes', value: 'Recipes' },
+      { name: 'Shortcuts', value: 'Shortcuts' },
+      { name: 'SocialMedia', value: 'SocialMedia' },
+      { name: 'StJude', value: 'StJude' },
+      { name: 'ThingsILike', value: 'ThingsILike' },
+      { name: 'TV', value: 'TV' },
+      { name: 'WeblogPoMo', value: 'WeblogPoMo' },
+      { name: 'YearInReview', value: 'YearInReview' },
+    ],
+  });
 
-    const project = await utils.selectProject(__siteroot, {
-        title: 'none',
-        value: 'none',
-        description: 'none',
-    })
+  const project = await utils.selectProject(__siteroot, {
+    title: 'none',
+    value: 'none',
+    description: 'none',
+  });
 
-    let meta = `---
+  let meta = `---
 title: "${title}"
 permalink: /blog/${slug}/index.html
 date: ${postDate}
-excerpt: ""`
+excerpt: ""`;
 
-    if (tags.length > 0)
-    {
-        meta = `${meta}\ntags:\n${tags.map(tag => `    - ${tag}`).join('\n')}`
-    }
+  if (tags.length > 0) {
+    meta = `${meta}\ntags:\n${tags.map(tag => `    - ${tag}`).join('\n')}`;
+  }
 
-    if (project.value !== 'none') {
-        meta = `${meta}\nproject: ${project.link}`
-    }
+  if (project.value !== 'none') {
+    meta = `${meta}\nproject: ${project.link}`;
+  }
 
-    meta = `${meta}\n---`
+  meta = `${meta}\n---`;
 
-    fs.writeFileSync(`${__siteroot}/src/content/posts/${year}/${slugDate}-${slug}.md`, meta, { flag: "wx" })
-}
+  fs.writeFileSync(`${__siteroot}/src/content/posts/${year}/${slugDate}-${slug}.md`, meta, {
+    flag: 'wx',
+  });
+};
