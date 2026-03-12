cac9f46e072111f2c65b21868bcfd9e3eea0d9fe
Bruno Pulis <brunopulis@protonmail.com>
2026-03-11 21:13:17 -0300

    fix TinaCMS

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/home/pulis/Projetos/brunopulis/tina/__generated__/.cache/1773273597618', url: 'https://content.tinajs.io/2.1/content/8aba382b-98b2-494f-93e5-c8cbd5d6de3b/github/main', token: 'd686461d60a8bd553e1996561adc32c395e5d688', queries,  });
+export const client = createClient({ cacheDir: '/home/pulis/Projetos/brunopulis/tina/__generated__/.cache/1773274287244', url: 'https://content.tinajs.io/2.1/content/8aba382b-98b2-494f-93e5-c8cbd5d6de3b/github/main', token: 'd686461d60a8bd553e1996561adc32c395e5d688', queries,  });
 export default client;
   
