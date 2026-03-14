fdffa231916ab59da4e70be58c495872512d27f8
Bruno Pulis <brunopulis@protonmail.com>
2026-03-14 20:51:42 -0300

    feat: tina generated

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,8 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ cacheDir: '/home/pulis/Projetos/brunopulis/tina/__generated__/.cache/1773274287244', url: 'https://content.tinajs.io/2.1/content/8aba382b-98b2-494f-93e5-c8cbd5d6de3b/github/main', token: 'd686461d60a8bd553e1996561adc32c395e5d688', queries,  });
+import { createClient } from 'tinacms/dist/client';
+import { queries } from './types';
+export const client = createClient({
+  url: 'http://localhost:4001/graphql',
+  token: 'd686461d60a8bd553e1996561adc32c395e5d688',
+  queries,
+});
 export default client;
-  
