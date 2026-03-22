16b2acb0021a5c0dd4db8e2c6b4ddc7a67046e29
Bruno Pulis <brunopulis@protonmail.com>
2026-03-22 19:52:33 -0300

    update configs

--- src/_config/plugins.js
+++ src/_config/plugins.js
@@ -1,14 +1,21 @@
+// Eleventy
 import {EleventyRenderPlugin} from '@11ty/eleventy';
-import {eleventyImageTransformPlugin} from '@11ty/eleventy-img';
 import rss from '@11ty/eleventy-plugin-rss';
+import syntaxHighlight from '@11ty/eleventy-plugin-syntaxhighlight';
 import webc from '@11ty/eleventy-plugin-webc';
-import {drafts} from './plugins/drafts.js';
-import {htmlConfig} from './plugins/html-config.js';
+import {eleventyImageTransformPlugin} from '@11ty/eleventy-img';
+
+// custom
 import {markdownLib} from './plugins/markdown.js';
+import {drafts} from './plugins/drafts.js';
+
+// Custom transforms
+import {htmlConfig} from './plugins/html-config.js';
 
 export default {
   EleventyRenderPlugin,
   rss,
+  syntaxHighlight,
   webc,
   eleventyImageTransformPlugin,
   markdownLib,
