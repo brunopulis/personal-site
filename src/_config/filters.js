8dc4f72a89f7dbc7eab225332c021b7aeef8b11d
Bruno Pulis <brunopulis@protonmail.com>
2026-04-05 20:19:03 -0300

    setup entire project

--- src/_config/filters.js
+++ src/_config/filters.js
@@ -1,59 +1,24 @@
-import {
-  formatDate,
-  readableDate,
-  relativeDate,
-  sortByDate,
-  toISOString,
-  toRfc822Date
-} from './filters/dates.js';
-import {filterByMediaType, lazyGroupByWatchedYear} from './filters/filterByType.js';
-import {filterByYear} from './filters/filterByYear.js';
-import {groupBy, groupByMonth, groupByYear, groupByYearMonth} from './filters/groupBy.js';
-import {getPostsByTag} from './filters/get-posts-by-tag.js';
-import {lazyGroupByYear, lazyPaginate} from './filters/lazy.js';
-import {limit} from './filters/limit.js';
+import {toISOString, formatDate} from './filters/dates.js';
 import {markdownFormat} from './filters/markdown-format.js';
-import {ogImage} from './filters/og-image.js';
-import {readingTime} from './filters/reading-time.js';
-import {shuffle} from './filters/shuffle.js';
-import {slugifyString, postSlug} from './filters/slugify.js';
+import {shuffleArray} from './filters/sort-random.js';
 import {sortAlphabetically} from './filters/sort-alphabetic.js';
-import {sortKeys} from './filters/sort-keys.js';
-import {splitStrings} from './filters/split-strings.js';
 import {splitlines} from './filters/splitlines.js';
-import {starRating} from './filters/starRating.js';
 import {striptags} from './filters/striptags.js';
+import {slugifyString} from './filters/slugify.js';
+import {limit} from './filters/limit.js';
+import {renderTransforms} from './filters/render-transforms.js';
+import {lastModified} from './filters/last-modified.js';
 
 export default {
-  groupBy,
-  groupByYear,
-  groupByMonth,
-  groupByYearMonth,
-  sortKeys,
   toISOString,
-  toRfc822Date,
   formatDate,
-  readableDate,
-  sortByDate,
-  filterByYear,
-  filterByMediaType,
-  relativeDate,
-  limit,
   markdownFormat,
-  markdownify: markdownFormat,
-  ogImage,
-  readingTime,
   splitlines,
   striptags,
-  shuffle,
+  shuffleArray,
   sortAlphabetically,
-  splitStrings,
-  slugify: slugifyString,
   slugifyString,
-  postSlug,
-  getPostsByTag,
-  lazyPaginate,
-  lazyGroupByYear,
-  lazyGroupByWatchedYear,
-  starRating
+  limit,
+  renderTransforms,
+  lastModified
 };
