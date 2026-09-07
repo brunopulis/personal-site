01d2a0316e3c19017df3ccc2768be2077b68a2b6
Bruno Pulis <brunopulis@protonmail.com>
2026-09-07 17:36:22 -0300

    Move draft post and add category taxonomy data
    Relocate the El Niño draft from the published content tree to
    src/drafts, where unfinished posts belong. Add a new taxonomy data file
    defining movie and TV categories by slug and label.

---
+++ src/_data/taxonomy.js
@@ -0,0 +1,19 @@
+export const categories = {
+  movies: [
+    {slug: 'acao', label: 'Ação'},
+    {slug: 'aventura', label: 'Aventura'},
+    {slug: 'comedia', label: 'Comédia'},
+    {slug: 'documentario', label: 'Documentário'},
+    {slug: 'ficcao', label: 'Ficção'}
+  ],
+
+  tv: [
+    {slug: 'acao', label: 'Ação'},
+    {slug: 'aventura', label: 'Aventura'},
+    {slug: 'comedia', label: 'Comédia'},
+    {slug: 'documentario', label: 'Documentário'},
+    {slug: 'ficcao', label: 'Ficção'}
+  ]
+};
+
+export default {categories};
