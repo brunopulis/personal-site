61f463d34f7dcda00fc8382f57d9f99469e24775
Bruno Pulis <brunopulis@protonmail.com>
2026-09-12 23:38:03 -0300

    Add toRfc822Date export to date filters
    Expose the RFC 822 date formatter from the date filter module so
    feed dates stay consistent with the rest of the date utilities.

--- src/_config/filters/dates.js
+++ src/_config/filters/dates.js
@@ -1,4 +1,13 @@
 import dayjs from 'dayjs';
+import {DateTime} from 'luxon';
 
 /** Formats a date using dayjs's conventions: https://day.js.org/docs/en/display/format */
 export const formatDate = (date, format) => dayjs(date).format(format);
+
+export const toRfc822Date = dateObj => {
+  try {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('EEE, dd LLL yyyy HH:mm:ss Z');
+  } catch {
+    return '';
+  }
+};
